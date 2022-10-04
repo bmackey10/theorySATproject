8fa2e79e3cef39fc619875ec73e152e0ec1a24a5
Jack Munhall <jman777jman@gmail.com>
2022-10-04 15:11:35 -0400

    added comma to split and fixed some output variables

--- brute.py
+++ brute.py
@@ -1,3 +1,4 @@
+from re import L
 import sys
 import csv
 
@@ -18,42 +19,62 @@ def verify(i, wff):
         clauseTruth = False
         for num in clause:
             if num < 0:
-                if not (i >> abs(clause)-1 & 1):
+                if not ((i >> (abs(clause)-1)) & 1):
                     clauseTruth = True
             if num > 0:
-                if (i >> abs(clause)-1 & 1):
+                if ((i >> (abs(clause)-1)) & 1):
                     clauseTruth = True
-        return clauseTruth
+        if not clauseTruth:
+            return False
+    
+    return True
 
 def readWFF(line, file):
     # c line
     line = line.split()
-    problemNum, maxLiterals, satisfiable = line[1], line[2], None
+    problemNum, maxLiterals, satisfiableAns = line[1], line[2], None
 
     if len(line) == 4:
-        satisfiable = line[3]
+        satisfiableAns = line[3]
     
     # p line
     line = file.readline().split()
     numVars, numClauses = line[2], line[3]
 
-    # TODO: brute force
-    line = file.readline().split()
+    # reading in wff
+    line = file.readline().split(',')
+    print(line)
     wff = []
     while line and line[0] != 'c':
-        # UNSURE?: each wff will be stored like [[1, 2, 0], [2, -3, 0]]
+        # each wff will be stored like [[1, 2], [2, -3, 0]]
         line.pop()
         wff.append(line)
-        line = file.readline().split()
+        line = file.readline().split(',')
 
+    print(wff)
+    # call function verify on each assignment with the above wff
+    rightAns = None
     for i in range(1<<int(numVars)):
         if not verify(i, wff):
+            satisfiable = 'U'
+    if not rightAns:
+        satisfiable = 'S' 
 
-    # nextAssignment(numVars)
+    # get total # of literals
 
+    # setting 
+    if not satisfiableAns:
+        rightAns = 0
+    else:
+        if satisfiableAns == satisfiable:
+            rightAns = 1
+        else:
+            rightAns = -1
+
+    totalLiterals = None
     # output
-    totalLiterals, rightAns, execTime, values = None, None, None, None # dummy values for now 
-    output(problemNum, maxLiterals, totalLiterals, satisfiable, rightAns, execTime, values)
+    execTime, values = None, None # dummy values for now 
+    output(problemNum, numVars, numClauses, maxLiterals, totalLiterals, satisfiable, rightAns, execTime, values)
     
     # returning the next 'c' line back to readFile
     return ' '.join(line)
@@ -64,6 +85,7 @@ def readFile(fileName):
     while line:
         if line[0] == 'c':
             line = readWFF(line, file)
+        break
 
     file.close()
     f.close()
