7e2996df3e63c04d6873eca1870aa15b4be3f7bc
Brooke Mackey <bmackey@nd.edu>
2022-10-07 17:06:55 -0400

    comments

--- 2SAT.py
+++ 2SAT.py
@@ -8,23 +8,28 @@ writer = csv.writer(f)
 def output(args):
     writer.writerow(args)
 
-
+# Check all values that depend on the current values assignment
 def solver(wff, numVars, values, currValue, assignment):
 
     tempValues = values.copy()
 
+    # if the current value is in the values dictionary already, do not check
     if currValue not in tempValues:
         tempValues[currValue] = assignment
     else:
         return tempValues
 
+    # else check to see if current value is in the clauses
     for clause in wff:
         indexOther = -1
+        # if the current value is in the clause and false, then find the index of the other value
         if tempValues[currValue] == 0 and currValue in clause:
             indexOther = int(not(clause.index(currValue)))
         elif tempValues[currValue] == 1 and -currValue in clause:
             indexOther = int(not(clause.index(-currValue)))
 
+        # check to see if assignments are conflicting, and return {} if they are
+        # otherwise add it to the values dictionary
         if indexOther != -1:
             if clause[indexOther] < 0:
                 if -clause[indexOther] in tempValues and tempValues[-clause[indexOther]] != 0:
@@ -48,18 +53,23 @@ def helper(wff, numVars):
     currStack = [1]
     currValue = 1
 
+    # while the stack is not empty
     while (currStack):
         newValues = {}
         origValues = values.copy()
 
+        # test with the current Value being 0
         if currStackDict[currValue][0] == 0:
             newValues = solver(wff, numVars, values, currValue, 0)
             currStackDict[currValue][0] = 1
 
+        # test with the current Value being 1
         if newValues == {} and currStackDict[currValue][0] == 1:
             newValues = solver(wff, numVars, values, currValue, 1)
             currStackDict[currValue][0] = 2
 
+        # if both lead to conflicts, revert to the previous value in the stack
+        # otherwise find a value that is not in the values dictionary to test
         if newValues == {}:
             while len(currStack) > 0 and currStackDict[currStack[-1]][0] == 2:
                 discard = currStack.pop()
@@ -80,6 +90,7 @@ def helper(wff, numVars):
                 currStackDict[currValue][1] = origValues.copy()
                 currValue = currOptions[0]
             else:
+                # if the length of values is equal to the number of variables, test the assignments
                 [input, binaryVal] = concatBools(values, numVars)
                 if verify(input, wff):
                     return binaryVal
